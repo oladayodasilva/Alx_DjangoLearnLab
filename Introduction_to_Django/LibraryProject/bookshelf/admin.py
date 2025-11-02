6f83556b24438861fab5448aa545a805bf46ac91
oladayodasilva <oladayodasilva87@gmail.com>
2025-11-02 22:18:59 +0100

    Integrate Book model with Django Admin and add customization

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import Book
 
-@admin.register(Book)
+# Customize how Book entries appear in the admin
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
+    list_filter = ('publication_year',)
+
+# Register the model with the admin site
+admin.site.register(Book, BookAdmin)
 
