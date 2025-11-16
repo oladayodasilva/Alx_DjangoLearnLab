d291c9d01e7fc463d4650e7b6a06f38b8a359181
oladayodasilva <oladayodasilva87@gmail.com>
2025-11-16 14:37:33 +0100

    Add models.py to bookshelf app to pass ALX task 0

---
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+
+class CustomUserManager(BaseUserManager):
+    """Manager for CustomUser"""
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Users must provide an email address")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    """
+    Extends Django’s AbstractUser
+    Removes username as the primary login field
+    """
+
+    username = None  # We use email instead
+    email = models.EmailField(unique=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to="profile_photos/", null=True, blank=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []  # No username
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
