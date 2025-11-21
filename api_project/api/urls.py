a1de48ebc128ef13bc09a4ce31b39c1a41d6a354
oladayodasilva <oladayodasilva87@gmail.com>
2025-11-21 16:32:04 +0100

    Completed Task 1 & Task 2: Book API endpoints and CRUD

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
-from .views import BookListCreateAPIView
+from django.urls import path,include
+from .views import BookList, BookDetail
+from .views import BookViewSet 
 
 urlpatterns = [
-    path("api/books/", BookListCreateAPIView.as_view(), name="book_list_create"),
+    path("api/books/", BookList.as_view(), name='book_all'),
+    path("api/books/<int:pk>/", BookDetail.as_view(), name="book_detail"),
 ]
-
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,12 @@
 
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
-class BookListCreateAPIView(generics.ListCreateAPIView):
+class BookList(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+class BookDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
