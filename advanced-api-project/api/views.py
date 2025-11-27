609d242429de3ce78a3445b3de357a0fddb1f571
oladayodasilva <oladayodasilva87@gmail.com>
2025-11-27 22:12:42 +0100

    Added required permission imports for task 1

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,8 +1,30 @@
+from django.views.generic import ListView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Book
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book, Author
 from .serializers import BookSerializer, AuthorSerializer
 
+
+
+class BookListView(ListView):
+    model = Book
+    template_name = 'book_list.html'
+    context_object_name = 'books'
+
+
+class BookUpdateView(UpdateView):
+    model = Book
+    fields = ['title', 'description', 'publication_date', 'author']
+    template_name = 'book_form.html'
+    success_url = reverse_lazy('book-list')
+
+
+class BookDeleteView(DeleteView):
+    model = Book
+    template_name = 'book_confirm_delete.html'
+    success_url = reverse_lazy('book-list')
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
@@ -14,4 +36,3 @@ class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-
