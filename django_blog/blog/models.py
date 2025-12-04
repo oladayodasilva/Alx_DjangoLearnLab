539ad5242c9a83f40a0483d49e80a18901cc0c72
oladayodasilva <oladayodasilva87@gmail.com>
2025-12-04 14:13:11 +0100

    Fix Post model for Task 0: added author and published_date

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    published_date = models.DateTimeField(null=True, blank=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
