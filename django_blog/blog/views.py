d1aab0e236efb5ac704b02f0ad08b56fe04c3056
oladayodasilva <oladayodasilva87@gmail.com>
2025-12-04 13:48:57 +0100

    Task 1: Implement User Authentication System

---
+++ django_blog/blog/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView, LogoutView
+from .forms import UserRegisterForm, UserUpdateForm
+
+
+class CustomLoginView(LoginView):
+    template_name = 'blog/login.html'
+
+
+class CustomLogoutView(LogoutView):
+    template_name = 'blog/logout.html'
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Auto-login after registration
+            return redirect('profile')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = UserUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = UserUpdateForm(instance=request.user)
+
+    return render(request, 'blog/profile.html', {'form': form})
+
