9855a2ae196003d9d89de881155bee42517587b9
oladayodasilva <oladayodasilva87@gmail.com>
2025-12-04 14:07:35 +0100

    Fix Task 0 checks: added blog app, Post model, db config placeholders

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -54,9 +54,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': '',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
