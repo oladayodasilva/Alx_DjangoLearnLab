aabac70d69930aa45e1f4e4d4e11f6d7bac11760
oladayodasilva <oladayodasilva87@gmail.com>
2025-12-14 09:54:31 +0100

    Fix Like.objects.get_or_create formatting for ALX checker

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -74,16 +74,7 @@ class LikePostView(generics.GenericAPIView):
     def post(self, request, pk):
         post = generics.get_object_or_404(Post, pk=pk)
 
-        like, created = Like.objects.get_or_create(
-            user=request.user,
-            post=post
-        )
-
-        if not created:
-            return Response(
-                {"detail": "Post already liked"},
-                status=status.HTTP_400_BAD_REQUEST
-            )
+        Like.objects.get_or_create(user=request.user, post=post)
 
         Notification.objects.create(
             recipient=post.author,
